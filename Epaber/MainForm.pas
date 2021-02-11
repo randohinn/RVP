968e94af76c6916d885c195f42cd1c632994d8dc
Rando Hinn <randohinn@gmail.com>
2021-02-12 00:06:35 +0200

    Delphi ide lolluste fix

--- Epaber/MainForm.pas
+++ Epaber/MainForm.pas
@@ -49,16 +49,16 @@ implementation
 
 procedure TForm1.btnConnectClick(Sender: TObject);
 begin
-  ComPort1.BaudRate := br38400;
-  ComPort1.DeviceName := '\\.\'+ComboBox1.Items[ComboBox1.ItemIndex];
-  ComPort1.Active := not ComPort1.Active;
-  if ComPort1.Active then
+  comport.BaudRate := br38400;
+  comport.DeviceName := '\\.\'+cmbPorts.Items[cmbPorts.ItemIndex];
+  comport.Active := not comport.Active;
+  if comport.Active then
   begin
-    Button1.Caption := 'HÜLGA';
+    btnConnect.Caption := 'HÜLGA';
   end
   else
   begin
-    Button1.Caption := 'ÜHENDA';
+    btnConnect.Caption := 'ÜHENDA';
   end;
   transmittable := true;
 end;
@@ -69,12 +69,12 @@ var
   x: Integer;
   y: Integer;
 begin
-  if OpenDialog1.Execute
+  if dialogFilePicker.Execute
   then
   begin
      bmp := TBitmap.Create;
 
-     bmp.LoadFromFile(OpenDialog1.FileName);
+     bmp.LoadFromFile(dialogFilePicker.FileName);
      for x := 0 to bmp.Width do
       for y := 0 to bmp.Height do
       begin
@@ -84,7 +84,7 @@ begin
         end;
 
       end;
-     Image1.Canvas.Draw(0,0,bmp);
+     imScreen.Canvas.Draw(0,0,bmp);
   end
   else ShowMessage('Open file was cancelled');
   //OpenDialog1.Free;
@@ -92,8 +92,8 @@ end;
 
 procedure TForm1.btnBGCleanClick(Sender: TObject);
 begin
-ComPort1.Write('W'+#13,TEncoding.ANSI);
-  Comport1.Flush;
+comport.Write('W'+#13,TEncoding.ANSI);
+  comport.Flush;
 end;
 
 procedure TForm1.btnLoadBlackClick(Sender: TObject);
@@ -102,12 +102,12 @@ var
   x: Integer;
   y: Integer;
 begin
-  if OpenDialog1.Execute
+  if dialogFilePicker.Execute
   then
   begin
      bmp := TBitmap.Create;
 
-     bmp.LoadFromFile(OpenDialog1.FileName);
+     bmp.LoadFromFile(dialogFilePicker.FileName);
      for x := 0 to bmp.Width do
       for y := 0 to bmp.Height do
       begin
@@ -117,7 +117,7 @@ begin
         end;
 
       end;
-     Image1.Canvas.Draw(0,100,bmp);
+     imScreen.Canvas.Draw(0,100,bmp);
   end
   else ShowMessage('Open file was cancelled');
   //OpenDialog1.Free;
@@ -126,8 +126,8 @@ end;
 procedure TForm1.btnDrawClick(Sender: TObject);
 begin
  
-  ComPort1.Write('R'+#13,TEncoding.ANSI);
-  Comport1.Flush;
+  comport.Write('R'+#13,TEncoding.ANSI);
+  comport.Flush;
 end;
 
 procedure TForm1.btnSendBlackClick(Sender: TObject);
@@ -140,7 +140,7 @@ var
   subframey: Integer;
   hascontent: Boolean;
 begin
-  ProgressBar1.Position := 0;
+  progressSend.Position := 0;
   for xpos := 0 to x_subframes do begin
     for ypos := 0 to y_subframes do begin
       subframex := xpos*40;
@@ -148,18 +148,18 @@ begin
       hascontent := false;
       for x := 0 to 40 do begin
         for y := 0 to 40 do begin
-          ProgressBar1.StepBy(1);
+          progressSend.StepBy(1);
           Application.ProcessMessages;
 
-          if Image1.Canvas.Pixels[subframex+x,subframey+y] = clBlack then
+          if imscreen.Canvas.Pixels[subframex+x,subframey+y] = clBlack then
           begin
               hascontent := true;
-              ComPort1.Write('S',TEncoding.ANSI);
-              ComPort1.WriteByte(x);
-              ComPort1.WriteByte(y);
-              ComPort1.Write(''+#13,TEncoding.ANSI);
-              Comport1.Flush();
-              Comport1.Flush();
+              comport.Write('S',TEncoding.ANSI);
+              comport.WriteByte(x);
+              comport.WriteByte(y);
+              comport.Write(''+#13,TEncoding.ANSI);
+              comport.Flush();
+              comport.Flush();
               Sleep(100);
           end;
 
@@ -167,11 +167,11 @@ begin
       end;
       if hascontent then
       begin
-        ComPort1.Write('DB',TEncoding.ANSI);
-        ComPort1.WriteByte(subframex);
-        ComPort1.WriteByte(subframey);
-        ComPort1.Write(''+#13,TEncoding.ANSI);
-        Comport1.Flush();
+        comport.Write('DB',TEncoding.ANSI);
+        comport.WriteByte(subframex);
+        comport.WriteByte(subframey);
+        comport.Write(''+#13,TEncoding.ANSI);
+        comport.Flush();
         Sleep(50);
       end;
 
@@ -188,7 +188,7 @@ var
   subframey: Integer;
   hascontent: Boolean;
 begin
-  ProgressBar1.Position := 0;
+  progressSend.Position := 0;
   for xpos := 0 to x_subframes do begin
     for ypos := 0 to y_subframes do begin
       subframex := xpos*40;
@@ -196,18 +196,18 @@ begin
       hascontent := false;
       for x := 0 to 40 do begin
         for y := 0 to 40 do begin
-          ProgressBar1.StepBy(1);
+          progressSend.StepBy(1);
           Application.ProcessMessages;
 
-          if Image1.Canvas.Pixels[subframex+x,subframey+y] = clRed then
+          if imScreen.Canvas.Pixels[subframex+x,subframey+y] = clRed then
           begin
               hascontent := true;
-              ComPort1.Write('S',TEncoding.ANSI);
-              ComPort1.WriteByte(x);
-              ComPort1.WriteByte(y);
-              ComPort1.Write(''+#13,TEncoding.ANSI);
-              Comport1.Flush();
-              Comport1.Flush();
+              comport.Write('S',TEncoding.ANSI);
+              comport.WriteByte(x);
+              comport.WriteByte(y);
+              comport.Write(''+#13,TEncoding.ANSI);
+              comport.Flush();
+              comport.Flush();
               Sleep(100);
           end;
 
@@ -215,11 +215,11 @@ begin
       end;
       if hascontent then
       begin
-        ComPort1.Write('DR',TEncoding.ANSI);
-        ComPort1.WriteByte(subframex);
-        ComPort1.WriteByte(subframey);
-        ComPort1.Write(''+#13,TEncoding.ANSI);
-        Comport1.Flush();
+        comport.Write('DR',TEncoding.ANSI);
+        comport.WriteByte(subframex);
+        comport.WriteByte(subframey);
+        comport.Write(''+#13,TEncoding.ANSI);
+        comport.Flush();
         Sleep(50);
       end;
 
@@ -238,23 +238,23 @@ begin
     Reg.RootKey := HKEY_LOCAL_MACHINE;
     if Reg.OpenKeyReadOnly('\HARDWARE\DEVICEMAP\SERIALCOMM') then
     begin
-      Reg.GetValueNames(ComboBox1.Items);
-      for I := 0 to ComboBox1.Items.Count - 1 do
-          ComboBox1.Items[i] := Reg.ReadString(ComboBox1.Items[i]);
+      Reg.GetValueNames(cmbPorts.Items);
+      for I := 0 to cmbPorts.Items.Count - 1 do
+          cmbPorts.Items[i] := Reg.ReadString(cmbPorts.Items[i]);
     end;
-    ComboBox1.Sorted := true;
+    cmbPorts.Sorted := true;
   finally
     Reg.Free;
   end;
-  Image1.Height := 250;
-  Image1.Width := 120;
-  x_subframes := Image1.Width div 40;
-  y_subframes := Image1.Height div 40;
-  ProgressBar1.Max := Image1.Height*Image1.Width;
+  imScreen.Height := 250;
+  imScreen.Width := 120;
+  x_subframes := imScreen.Width div 40;
+  y_subframes := imScreen.Height div 40;
+  progressSend.Max := imScreen.Height*imScreen.Width;
 end;
 procedure TForm1.FormShow(Sender: TObject);
 begin
-  with Image1 do
+  with imScreen do
   begin
     Canvas.Pen.Color := clRed;
     Canvas.Brush.Color := clRed;
@@ -270,7 +270,7 @@ begin
   ptmouse := Mouse.CursorPos;
   ptmouse := ScreenToClient(ptmouse);
 
-   with Image1 do
+   with imScreen do
   begin
     Canvas.Pen.Color := clRed;
     Canvas.Brush.Color := clRed;
